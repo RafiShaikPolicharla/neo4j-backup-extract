17b0e9a93647ab66044888680886fdfbe21bdf59
Rafi.Shaik <rafi.shaik@tigeranalytics.com>
2025-07-01 14:32:16 +0530

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,1 +1,17 @@
+from neo4j import GraphDatabase
+from neo4j_backup import Extractor
+
+uri = "bolt://<YOUR-BOLT-URL>"
+username = "<USERNAME>"
+password = "<PASSWORD>"
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+extractor = Extractor(
+    project_dir="data_dump",
+    driver=driver,
+    database="neo4j",
+    compress=False,
+    input_yes=True,
+)
+extractor.extract_data()
 
